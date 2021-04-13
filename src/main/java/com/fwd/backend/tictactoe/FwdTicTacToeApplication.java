046ee929a960db18d63d64de4722ad1240d31105
steven.yeo <steven.yeo@velocity-solutions.com>
2021-04-13 10:27:25 +0700

    20210413 - Initialize Simple Java Web Application

---
+++ src/main/java/com/fwd/backend/tictactoe/FwdTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.fwd.backend.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwdTicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwdTicTacToeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fwd/backend/tictactoe/FwdTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwd.backend.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwdTicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
